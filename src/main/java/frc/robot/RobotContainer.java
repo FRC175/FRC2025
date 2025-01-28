d0154d499370955470bfdbc8099a1c864e6baab9
CharlesTheEpicMinecraftPlayer <IAmACursedFurby@gmail.com>
2025-01-27 20:14:00 -0500

    Swerve sort of working as long as we only ever go in straight lines

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,24 +27,13 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.ArmConstants;
-import frc.robot.Constants.ArmPosition;
 import frc.robot.Constants.ControllerConstants;
-import frc.robot.Constants.LiftConstants;
 
-import frc.robot.commands.LED.BlinkAlliance;
-import frc.robot.commands.LED.BlinkCycle;
-import frc.robot.commands.LED.SetDuring;
-
-import frc.robot.subsystems.LED.LEDColor;
 import frc.robot.utils.Controller;
 import frc.robot.utils.Utils;
 
-import frc.robot.subsystems.LED;
-import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Drive.SwerveSubsystem;
-import frc.robot.subsystems.Lift;
-import frc.robot.subsystems.Limelight;
+
 
 
 /**
@@ -57,23 +46,14 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   // private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
- // private final Intake intake; 
-  private final Shooter shooter; 
-  private final Limelight limelight;
-  private final Lift lift; 
-  // private final Arm arm; 
-  private final LED led;
+; 
+  
   private final Controller driverController/* , operatorController*/;
   private final XboxController operatorController;
   private final SendableChooser<Command> autoChooser;
 
   private final SwerveSubsystem drive = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
   
-  PrintWriter writer;
-  
-
-  double lockAngle = 0;
 
   private static RobotContainer instance;
 
@@ -82,15 +62,8 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
   
-    // intake = Intake.getInstance(); 
-    shooter = Shooter.getInstance();
-    limelight = Limelight.getInstance();
-    lift = Lift.getInstance(); 
-    // arm = Arm.getInstance();
-    led = LED.getInstance();
 
     driverController = new Controller(new Joystick(ControllerConstants.DRIVER_CONTROLLER_PORT));
-    //operatorController = new Joystick(ControllerConstants.OPERATOR_CONTROLLER_PORT);
     operatorController = new XboxController(ControllerConstants.OPERATOR_CONTROLLER_PORT);
 
     autoChooser = new SendableChooser<>();
@@ -116,14 +89,13 @@ public class RobotContainer {
   }
 
   private void configureDefaultCommands() {
-    drive.driveCommand(
-       () -> MathUtil.applyDeadband(driverController.getLeftX(), Constants.DriveConstants.driveDeadbandX, Constants.DriveConstants.MAXIMUMSPEED),
-       () -> MathUtil.applyDeadband(driverController.getLeftY(), Constants.DriveConstants.driveDeadbandX, Constants.DriveConstants.MAXIMUMSPEED),
-       () -> driverController.getTwist());
-
-      led.setDefaultCommand(new BlinkAlliance());
+   
+    drive.setDefaultCommand(drive.driveCommand(() ->
+     MathUtil.applyDeadband(driverController.getLeftX(), Constants.DriveConstants.driveDeadbandX, Constants.DriveConstants.MAXIMUMSPEED),
+    () -> MathUtil.applyDeadband(driverController.getLeftY(), Constants.DriveConstants.driveDeadbandX, Constants.DriveConstants.MAXIMUMSPEED),
+    () -> driverController.getTwist()));
+     
   }
-
 
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
@@ -133,142 +105,6 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
 
-  
-      // .onFalse(new Swerve(driverController, drive));
-     
-
-    // Operator Controller X Button: Reverse Intake 
-   
-      
-
-    // Operator Controller B Button: Amp 
-    // new Trigger(() -> operatorController.getBButton())
-    //   .onTrue(new Amp(intake, shooter, 0.4))
-    //   .onFalse(new Amp(intake, shooter, 0));
-    
-    // // Operator Controller B Button: Shoot in Speaker 
-    // new Trigger(() -> operatorController.getBButton() )
-    // .onTrue(new InstantCommand(() -> {
-    // shooter.shooterSetOpenLoop(0.5,0.5);
-    // // System.out.println("const");
-    // }, shooter))
-    // .onFalse(new InstantCommand(() -> {
-    //   shooter.shooterSetOpenLoop(0.0, 0.0);
-    // }, shooter));
-
-    // Operator Controller Left Trigger: Intake 
-    // new Trigger(() -> operatorController.getLeftTriggerAxis() > 0.2)
-    // .onTrue(new pickup(intake, false))
-    // .whileTrue(new RunCommand(() -> {
-    //   if (intake.isNoteHeld()) {
-        // System.out.println("present");
-        
-      //   operatorController.setRumble(RumbleType.kBothRumble, 1.0);
-      // } else {
-      //   // System.out.println("not present");
-      //   operatorController.setRumble(RumbleType.kBothRumble, 0);
-      // }}))
-    // .onTrue(new RunCommand(() -> {
-
-    //   if (intake.isNotePresent()) {
-    //     operatorController.setRumble(RumbleType.kBothRumble, 1.0);
-    //     intake.setOpenLoop(0);
-    //   } else {
-    //     intake.setOpenLoop(0.5);
-    //     operatorController.setRumble(RumbleType.kBothRumble, 0);
-    //   }
-    // }))
-    // .onFalse(new InstantCommand(() -> {
-    //   intake.setOpenLoop(0);
-    //   operatorController.setRumble(RumbleType.kBothRumble, 0);
-    // }, intake));
-
-    // new Trigger(() -> intake.isNoteHeld())
-    //   .whileTrue(new BlinkCycle(1, 5, new LEDColor[] {LEDColor.ORANGE, DriverStation.getAlliance().get() == Alliance.Red ? LEDColor.RED : LEDColor.BLUE}))
-    //   .onFalse(new BlinkAlliance());
-
-
-    // new Trigger(() -> operatorController.getLeftBumper()) 
-    // .onTrue(new RevShooter(shooter, 0.8 * 3600, 0.8 * 2800))
-    // .onFalse(new InstantCommand(() -> {
-    //   shooter.shooterSetOpenLoop(0, 0);
-    // }, shooter));
-
-    // new Trigger(() -> operatorController.getRightTriggerAxis() > 0.5)
-    // .onTrue(new RevShooter(shooter, 3500, 3500)) // 51 42
-    // .onFalse(new InstantCommand(() -> {
-    //   shooter.shooterSetOpenLoop(0, 0);
-    // }, shooter));
-
-    // new Trigger(() -> operatorController.getAButton())
-    //   .onTrue(new InstantCommand(() -> intake.setOpenLoop(0.4), intake))
-    //   .onFalse(new InstantCommand(() -> intake.setOpenLoop(0), intake));
-
-    // // Operator DPad Down: Set Arm Intake Position
-    // new Trigger(() -> operatorController.getPOV() == 180)
-    //   // .onTrue(new SetArmPosition(arm, 0.2, false, ArmConstants.INTAKE));
-    //   .onTrue(new InstantCommand(() -> {
-    //     arm.setArmGoalPosition(ArmPosition.INTAKE);
-    //   }));
-
-    // new Trigger(() -> Math.abs(operatorController.getRightY()) > 0.5)
-    //   .whileTrue(new RunCommand(() -> {
-    //     if (operatorController.getRightY() < 0) {
-    //         arm.setArmGoalPosition(arm.getArmGoalPosition() - 0.001);
-    //     } else if (operatorController.getRightY() > 0) {
-    //       arm.setArmGoalPosition(arm.getArmGoalPosition() + 0.001);
-    //     }
-
-        
-    //   }));
-      // new Trigger(() -> operatorController.getLeftStickButton())
-      // .onTrue(new BlinkCycle(1, 5, new LEDColor[] {LEDColor.ORANGE, LEDColor.BLACK}))
-      // .onFalse(new BlinkAlliance());
-      
-      
-
-   //  Operator Dpad Up: Set Arm Amp Position 
-    // new Trigger(() -> operatorController.getPOV() == 90)
-    //   .onTrue(new InstantCommand(() -> {arm.setArmGoalPosition(ArmPosition.AMP);}));
-
-    // // Operator DPad Right: Set Arm Speaker Position 
-    // new Trigger(() -> operatorController.getPOV() == 0)
-    //   .onTrue(new ParallelCommandGroup ( new InstantCommand(() -> {
-    //     arm.setArmGoalPosition(ArmPosition.SPEAKER);}),
-    //     new SetDuring(4, LEDColor.PARTYMODE)
-    //   ))
-    //   .onFalse(new BlinkAlliance());
-      
-
-    // Operator DPad Right: Set Arm Speaker Position 
-    // new Trigger(() -> operatorController.getPOV() == 45)
-    //   .onTrue(new InstantCommand(() -> {arm.setArmGoalPosition(ArmPosition.TRAP);}));
-
-    // // Operator DPad Left: Set Arm Rest Position
-    // new Trigger(() -> operatorController.getPOV() == 270)
-    //   // .onTrue(new SetArmPosition(arm, 0.2, false, ArmConstants.REST));
-    //   .onTrue(new InstantCommand(() -> {arm.setArmGoalPosition(ArmPosition.PASSING);}));
-
-    // new Trigger(() -> operatorController.getYButton())
-    //     .onTrue(new InstantCommand(() -> {arm.setArmGoalPosition(ArmPosition.WHATEVER);}));
-
-    // new Trigger(() -> operatorController.getLeftBumper())
-    //   .onTrue(new RevShooter(shooter,4500, 4500)) // 51 42
-    //   .onFalse(new InstantCommand(() -> {
-    //   shooter.shooterSetOpenLoop(0, 0);
-    // }, shooter));
-
-    // new Trigger(() -> operatorController.getRightBumper())
-    //   .onTrue(new ArmViaLimelight(limelight, arm, 1));
-
-    // new Trigger(() -> operatorController.getRightStickButton())
-    //   .onTrue(new InstantCommand(() -> {lift.setLeftGoalPosition(lift.getFINAL_POSITION_LEFT()); lift.setRightGoalPosition(lift.getFINAL_POSITION_RIGHT()); lift.setBreak(false);}));
-
-    // new Trigger(() -> operatorController.getLeftStickButton())
-    //   .onTrue(new InstantCommand(() -> {lift.setLeftGoalPosition(lift.getSTARTING_POSITION_LEFT()); lift.setRightGoalPosition(lift.getSTARTING_POSITION_RIGHT()); lift.setBreak(false);}));
-
-    // new Trigger(() -> operatorController.getBackButton())
-    //   .onTrue(new InstantCommand(() -> {lift.setLeftGoalPosition(LiftConstants.FINAL_POSITION_LEFT); lift.setRightGoalPosition(LiftConstants.FINAL_POSITION_RIGHT);}));
   }
 
   private void configureAutoChooser() {
--- src/main/java/frc/robot/subsystems/Drive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Drive/SwerveSubsystem.java
@@ -2,18 +2,17 @@ package frc.robot.subsystems.Drive;
 
 import static edu.wpi.first.units.Units.Meter;
 
-
 import java.io.File;
-import java.io.IOException;
 import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+
 import swervelib.parser.SwerveControllerConfiguration;
 import swervelib.parser.SwerveDriveConfiguration;
 import swervelib.parser.SwerveParser;
 import swervelib.SwerveDrive;
+import swervelib.SwerveModule;
 import swervelib.math.SwerveMath;
 import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
@@ -21,20 +20,20 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.util.Units;
 import frc.robot.Constants.DriveConstants;
-
-
 
 
 public class SwerveSubsystem extends SubsystemBase {
 
-    private final double maximumSpeed = DriveConstants.MAXIMUMSPEED;
-    private final File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
-    private final SwerveDrive  swerveDrive;
+  private final double maximumSpeed = DriveConstants.MAXIMUMSPEED;
+  // SparkMax frontRight, frontLeft;
+  // RelativeEncoder FLencoder, FRencoder;
+
+  private final File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+  private final SwerveDrive  swerveDrive;
    
      
-      /**
+  /**
    * Command to drive the robot using translative values and heading as a setpoint.
    *
    * @param translationX Translation in the X direction.
@@ -45,34 +44,40 @@ public class SwerveSubsystem extends SubsystemBase {
    */
 
 
-   public SwerveSubsystem(File directory) {
+  public SwerveSubsystem(File directory) {
 
-    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
-
-    try
-    {
-      swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed,
-                                                                  new Pose2d(new Translation2d(Meter.of(1),
-                                                                                               Meter.of(4)),
-                                                                             Rotation2d.fromDegrees(0)));
-      // Alternative method if you don't want to supply the conversion factor via JSON files.
-      // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
-    } catch (Exception e)
-    {
-      throw new RuntimeException(e);
-    }
-    swerveDrive.setHeadingCorrection(false); // Heading correction should only be used while controlling the robot via angle.
-    swerveDrive.setCosineCompensator(false);//!SwerveDriveTelemetry.isSimulation); // Disables cosine compensation for simulations since it causes discrepancies not seen in real life.
-    swerveDrive.setAngularVelocityCompensation(true,
-                                               true,
-                                               0.1); //Correct for skew that gets worse as angular velocity increases. Start with a coefficient of 0.1.
-    swerveDrive.setModuleEncoderAutoSynchronize(false,
-                                                1);
+    // SparkMax frontRight = new SparkMax(2, MotorType.kBrushless);
+    // SparkMax frontLeft = new SparkMax(8, MotorType.kBrushless);
     
+    // RelativeEncoder FLencoder = frontLeft.getEncoder();
+    // RelativeEncoder FRencoder = frontRight.getEncoder();
 
-   }
+  SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
 
-   public SwerveSubsystem(SwerveDriveConfiguration driveCfg, SwerveControllerConfiguration controllerCfg)
+  try
+  {
+    swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed,
+                                                                new Pose2d(new Translation2d(Meter.of(1),
+                                                                                              Meter.of(4)),
+                                                                            Rotation2d.fromDegrees(0)));
+    // Alternative method if you don't want to supply the conversion factor via JSON files.
+    // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
+  } catch (Exception e)
+  {
+    throw new RuntimeException(e);
+  }
+  swerveDrive.setHeadingCorrection(false); // Heading correction should only be used while controlling the robot via angle.
+  swerveDrive.setCosineCompensator(false);//!SwerveDriveTelemetry.isSimulation); // Disables cosine compensation for simulations since it causes discrepancies not seen in real life.
+  swerveDrive.setAngularVelocityCompensation(true,
+                                              true,
+                                              0.1); //Correct for skew that gets worse as angular velocity increases. Start with a coefficient of 0.1.
+  swerveDrive.setModuleEncoderAutoSynchronize(false,
+                                              1);
+  
+
+  }
+
+  public SwerveSubsystem(SwerveDriveConfiguration driveCfg, SwerveControllerConfiguration controllerCfg)
   {
     swerveDrive = new SwerveDrive(driveCfg,
                                   controllerCfg,
@@ -109,17 +114,27 @@ public class SwerveSubsystem extends SubsystemBase {
    */
   public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX)
   {
+
+    // System.out.println(FLencoder.getPosition() + " " + FRencoder.getPosition());
+    
     return run(() -> {
-      // Make the robot move
+      SwerveModule[] states = swerveDrive.getModules();
+      for (int i = 0; i < states.length; ++i) {
+        System.out.print(i + ": " + states[i].getRawAbsolutePosition() + " ~~~ ");
+      }
+      System.out.println();
+
+      System.out.println(angularRotationX.getAsDouble() * swerveDrive.getMaximumChassisAngularVelocity());
       swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumChassisVelocity(),
                                           translationY.getAsDouble() * swerveDrive.getMaximumChassisVelocity()),
                         angularRotationX.getAsDouble() * swerveDrive.getMaximumChassisAngularVelocity(),
                         true,
                         false);
     });
+    
   }
 
-    public void driveFieldOriented(ChassisSpeeds velocity)
+  public void driveFieldOriented(ChassisSpeeds velocity)
   {
     swerveDrive.driveFieldOriented(velocity);
   }

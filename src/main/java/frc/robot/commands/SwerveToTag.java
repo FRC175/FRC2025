933eac71afc89b905b95e176393e39a67881eed4
CharlesTheEpicMinecraftPlayer <IAmACursedFurby@gmail.com>
2025-02-27 19:50:51 -0500

    fixed manipulator, swerveToTag implemented

---
+++ src/main/java/frc/robot/commands/SwerveToTag.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import java.lang.annotation.Target;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.PhotonVision;
+
+
+public class SwerveToTag extends Command{
+   private SwerveSubsystem swerveSubsystem;
+   private PhotonVision photonVision;
+   private Pose2d targetPos;
+   
+    public SwerveToTag(SwerveSubsystem swerveSubsystem) {
+        this.swerveSubsystem = swerveSubsystem;
+        photonVision = swerveSubsystem.setupPhotonVisionObject();
+        
+
+      
+     
+    }
+
+    @Override
+    public void execute() {
+        int target = photonVision.getPrimaryTargetId();
+        if (!(target == -1)) {
+            
+            targetPos = PhotonVision.getAprilTagPose(target, null);
+            swerveSubsystem.driveToPose(targetPos);
+        //need to tune!
+        }
+      
+    }
+    
+    
+    @Override
+    public boolean isFinished() {
+      return(swerveSubsystem.getPose() == targetPos);
+    }
+
+}
